f009e28464042fa40b381d93e0b4929e9f15e1b8
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-06 12:00:20 +0100

    en el script del slider es donde se castean las imagenes

--- transportes-fernandez/src/slider.jsx
+++ transportes-fernandez/src/slider.jsx
@@ -1,5 +1,23 @@
-$(document).ready(function(){
-    $('.slider').slick({
+$(document).ready(function() {
+    // Lista de rutas de imágenes
+    const imageList = [
+        "/src/assets/images/camion_resized.png",
+        "/src/assets/images/camion2_resized.jpg",
+        "/src/assets/images/fondo_mar_resized.jpg",
+        "/src/assets/images/awa.jpg"
+    ];
+
+    // Selección del contenedor del slider
+    const $sliderContainer = $('.slider');
+
+    // Generación de los elementos div con imágenes dentro del contenedor
+    imageList.forEach(src => {
+        const imageDiv = `<div><img src="${src}" alt="Imagen"></div>`;
+        $sliderContainer.append(imageDiv);
+    });
+
+    // Inicialización de slick
+    $sliderContainer.slick({
         arrows: true,       // Activa las flechas de navegación
         autoplay: false,    // Desactiva el cambio automático de imágenes
         prevArrow: '<button type="button" class="slick-prev custom-arrow"><i class="fas fa-arrow-left"></i></button>',
