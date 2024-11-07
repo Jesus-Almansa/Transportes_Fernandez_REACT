868f7afffd472a8e6f29b6c5b304709b62daa442
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-07 17:48:53 +0100

    a

--- transportes-fernandez/src/screens/Home.jsx
+++ transportes-fernandez/src/screens/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
     <div id=''>
       <Carousel />
       <h1>Welcome to Transportes Fernandez</h1>
-      <p>This is the Home page where you can find general information.</p>
+      <p>This is the Home page where you can find general information. Very good info</p>
       
     </div>
   );
