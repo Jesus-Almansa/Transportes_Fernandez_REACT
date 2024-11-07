4a37f0ed3904dba014069a87a239d40990fb95e8
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-07 17:28:29 +0100

    Refactor del nombre del Carrusel de Slider a Carousel

--- transportes-fernandez/src/screens/Home.jsx
+++ transportes-fernandez/src/screens/Home.jsx
@@ -3,13 +3,13 @@ import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import MyCarousel from '../components/Slider';
+import Carousel from '../components/Carousel';
 
 
 function Home() {
   return (
     <div id=''>
-      <MyCarousel />
+      <Carousel />
       <h1>Welcome to Transportes Fernandez</h1>
       <p>This is the Home page where you can find general information.</p>
       
