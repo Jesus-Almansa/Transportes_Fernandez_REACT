cd6cb8507fa1ffa81e544bcf3765dbac1ed33e29
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-11 18:44:31 +0100

    empleados actualizados

---
+++ transportes-fernandez/src/data/employees.js
@@ -0,0 +1,29 @@
+const employees = [
+    {
+      id: 1,
+      name: "Paz Fernández Lozano",
+      position: "CEO",
+      imageUrl: "https://unavatar.io/github/Jesus-Almansa",
+    },
+    {
+      id: 2,
+      name: "El primiyo chico",
+      position: "CEO de la vida",
+      imageUrl: "https://unavatar.io/github/torvalds",
+    },
+    {
+      id: 3,
+      name: "El tio rojo",
+      position: "Cazador de hunters",
+      imageUrl: "https://unavatar.io/github/rauulpastor",
+    },
+    {
+      id: 4,
+      name: "Nico",
+      position: "Data lover",
+      imageUrl: "https://unavatar.io/github/NicolasVollmer",
+    },
+  ];
+  
+  export default employees;
+  
