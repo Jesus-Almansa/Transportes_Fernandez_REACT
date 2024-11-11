b2f12466903f19b44f61abf2cee4f9c3574f90d7
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-11 18:32:05 +0100

    rutas

---
+++ transportes-fernandez/src/data/routes.js
@@ -0,0 +1,13 @@
+const routes = {
+    home: '/',
+    about: '/about',
+    services: '/services',
+    contact: '/contact',
+    login: '/login',
+    register: '/register',
+    profile: '/profile',
+    bookings: '/bookings',
+    tracking: '/tracking',
+};
+
+export default routes;
