370b8fd052730cac3df8436e68dc78e9a42cff48
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-14 17:17:45 +0100

    nuevos empleados

--- transportes-fernandez/src/data/employees.js
+++ transportes-fernandez/src/data/employees.js
@@ -1,29 +1,34 @@
 const employees = [
-    {
-      id: 1,
-      name: "Jesús Almansa",
-      position: "CTO",
-      imageUrl: "https://unavatar.io/github/Jesus-Almansa",
-    },
-    {
-      id: 2,
-      name: "El primiyo chico",
-      position: "Linux Enjoyer",
-      imageUrl: "https://unavatar.io/github/torvalds",
-    },
-    {
-      id: 3,
-      name: "El tio rojo",
-      position: "Cazador de hunters",
-      imageUrl: "https://unavatar.io/github/rauulpastor",
-    },
-    {
-      id: 4,
-      name: "Nico",
-      position: "Data lover",
-      imageUrl: "https://unavatar.io/github/NicolasVollmer",
-    },
-  ];
-  
-  export default employees;
-  
+  {
+    id: 1,
+    name: "Not Juan",
+    position: "CTO",
+    imageUrl: "https://unavatar.io/github/Jesus-Almansa",
+  },
+  {
+    id: 2,
+    name: "El primiyo chico",
+    position: "Linux Enjoyer",
+    imageUrl: "https://unavatar.io/github/torvalds",
+  },
+  {
+    id: 3,
+    name: "El tio rojo",
+    position: "Cazador de hunters",
+    imageUrl: "https://unavatar.io/github/rauulpastor",
+  },
+  {
+    id: 4,
+    name: "Jess",
+    position: "Data lover",
+    imageUrl: "https://unavatar.io/github/jfrazelle",
+  },
+  {
+    id: 5,
+    name: "Begum",
+    position: "Web lover",
+    imageUrl: "https://unavatar.io/github/begum",
+  },
+];
+
+export default employees;
