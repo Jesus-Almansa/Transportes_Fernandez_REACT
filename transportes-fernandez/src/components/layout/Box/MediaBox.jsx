28543515fad2525cc5b28dd7cc02672a319ba82a
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-18 13:27:38 +0100

    Ahora podemos meter videos en la página web, tanto de enlace de Youtube como archivo local

--- transportes-fernandez/src/components/layout/Box/MediaBox.jsx
+++ transportes-fernandez/src/components/layout/Box/MediaBox.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
-import './Box.css';
+import './MediaBox.css';
 
-const MediaBox = ({ image, className }) => {
+const MediaBox = ({ content, className }) => {
+  const getYouTubeEmbedUrl = (url) => {
+    const urlObj = new URL(url);
+    let videoId = '';
+
+    if (urlObj.hostname.includes('youtube.com')) {
+      videoId = urlObj.searchParams.get('v');
+    } else if (urlObj.hostname.includes('youtu.be')) {
+      videoId = urlObj.pathname.slice(1);
+    }
+
+    return videoId ? `https://www.youtube.com/embed/${videoId}` : null;
+  };
+
+  const isYouTubeUrl = content.includes('youtube.com') || content.includes('youtu.be');
+  const isMp4File = content.endsWith('.mp4');
+  const embedUrl = isYouTubeUrl ? getYouTubeEmbedUrl(content) : null;
+
   return (
-    <div className={className}>
-      src={image}
+    <div className={`media-container ${className}`}>
+      <div className="media-wrapper">
+        {isMp4File ? (
+          <video className="media" controls>
+            <source src={content} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        ) : isYouTubeUrl && embedUrl ? (
+          <iframe
+            className="media"
+            src={embedUrl}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="YouTube Video"
+          ></iframe>
+        ) : (
+          <img className="media" src={content} alt="media" />
+        )}
+      </div>
     </div>
   );
 };
--- transportes-fernandez/src/screens/Services.jsx
+++ transportes-fernandez/src/screens/Services.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import BoxList from '../components/layout/Box/BoxList';
+import MediaBox from '../components/layout/Box/MediaBox';
+import zeldaVideo from '../assets/video/zelda.mp4';
+// import oiai from '../assets/video/oiai.mp4';
 
 const Services = () => {
     return (
@@ -11,7 +14,11 @@ const Services = () => {
                     <li>Internacional</li>
                     <li>Espacial</li>
                 </ul>
-                </BoxList>
+            </BoxList>
+
+            <MediaBox content={zeldaVideo} /> 
+            <MediaBox content="https://www.youtube.com/watch?v=olKjeHdOW3Y" />
+            {/* <MediaBox content={oiai} /> */}
         </div>
     );
 };
