0aa0f9ae8d3431973a508c947ee6954f32ec0f5f
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-18 13:35:37 +0100

    podemos meter los vídeos que queramos

--- transportes-fernandez/src/components/layout/Box/MediaBox.jsx
+++ transportes-fernandez/src/components/layout/Box/MediaBox.jsx
@@ -3,21 +3,22 @@ import './MediaBox.css';
 
 const MediaBox = ({ content, className }) => {
   const getYouTubeEmbedUrl = (url) => {
-    const urlObj = new URL(url);
-    let videoId = '';
-
-    if (urlObj.hostname.includes('youtube.com')) {
-      videoId = urlObj.searchParams.get('v');
-    } else if (urlObj.hostname.includes('youtu.be')) {
-      videoId = urlObj.pathname.slice(1);
+    try {
+      const urlObj = new URL(url);
+      if (urlObj.hostname.includes('youtube.com')) {
+        return `https://www.youtube.com/embed/${urlObj.searchParams.get('v')}`;
+      }
+      if (urlObj.hostname.includes('youtu.be')) {
+        return `https://www.youtube.com/embed/${urlObj.pathname.slice(1)}`;
+      }
+    } catch (error) {
+      console.error('Invalid URL:', url);
     }
-
-    return videoId ? `https://www.youtube.com/embed/${videoId}` : null;
+    return null;
   };
 
-  const isYouTubeUrl = content.includes('youtube.com') || content.includes('youtu.be');
+  const embedUrl = content.includes('youtube.com') || content.includes('youtu.be') ? getYouTubeEmbedUrl(content) : null;
   const isMp4File = content.endsWith('.mp4');
-  const embedUrl = isYouTubeUrl ? getYouTubeEmbedUrl(content) : null;
 
   return (
     <div className={`media-container ${className}`}>
@@ -27,15 +28,15 @@ const MediaBox = ({ content, className }) => {
             <source src={content} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-        ) : isYouTubeUrl && embedUrl ? (
+        ) : embedUrl ? (
           <iframe
             className="media"
             src={embedUrl}
-            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             title="YouTube Video"
           ></iframe>
+
         ) : (
           <img className="media" src={content} alt="media" />
         )}
