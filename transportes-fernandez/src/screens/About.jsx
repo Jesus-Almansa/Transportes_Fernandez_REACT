12ef2daccd9b0ccdabc10dc0b0eea18158ad0f4a
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-21 17:39:10 +0100

    Probando los estilos para las cajas de /about

--- transportes-fernandez/src/screens/About.jsx
+++ transportes-fernandez/src/screens/About.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import { styled } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
-import Grid from '@mui/material/Grid'
-import Carousel from '../components/features/Carousel/Carousel'
+import React from 'react';
+import MediaPicture from '../components/features/MediaPicture/MediaPicture';
+import TextBox from '../components/layout/Box/TextBox';
+
+import laMancha from '../assets/original_images/Regional.png';
 
 function About() {
   return (
-    <div id='About'>
-      <h1>About Us</h1>
-      <Carousel />
-      <p>Learn more about our company and services.
-        TONTO EL QUE LO LEA
-      </p>
+    <div id="About">
+      <h1 className='alinearEnColumna'>Sobre nosotros</h1>
+      <br />
+      
+      <div className='alinearEnColumna'>
+        <div className="enLinea">
+          <div className="izquierda">
+            <MediaPicture src={laMancha} alt="placeholder" className='media_picture' />
+          </div>
+          <div className='derecha'>
+            <TextBox className='text-box-Empresa'>
+              Transportes Fernández is a family business that has been operating for over 40 years. We specialize in regional transportation, covering the area of La Mancha.
+            </TextBox>
+          </div>
+        </div>
+      </div>
+      <p>Learn more about our company and services. TONTO EL QUE LO LEA</p>
     </div>
-  )
+  );
 }
 
-export default About
-
+export default About;
