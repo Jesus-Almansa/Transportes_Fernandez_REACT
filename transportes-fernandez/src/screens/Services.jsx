4c012497108a33f9ca87b7d3805ebc5da0b03254
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-25 11:01:47 +0100

    refactor names

--- transportes-fernandez/src/screens/Services.jsx
+++ transportes-fernandez/src/screens/Services.jsx
@@ -3,7 +3,7 @@ import BoxList from '../components/layout/Box/BoxList';
 import MediaVideo from '../components/features/MediaVideo/MediaVideo.jsx';
 import MediaPicture from '../components/features/MediaPicture/MediaPicture.jsx';
 
-import FondoMar from '../assets/images/fondo_mar_resized.jpg';
+import regionalImage from '../assets/images/regional copy.png';
 import CiutatVella from '../assets/images/nacional.jpg';
 import zeldaVideo from '../assets/video/zelda.mp4';
 import OIAI from '../assets/video/OIAI.mp4';
@@ -12,7 +12,7 @@ const Services = () => {
     return (
         <div>
             <MediaPicture src={CiutatVella} alt="placeholder" />
-            {/* <MediaPicture src={FondoMar} alt="placeholder" /> */}
+            {/* <MediaPicture src={regionalImage} alt="placeholder" /> */}
             <BoxList title="Servicios">
                 <ul>
                     <li>Regional</li>
@@ -22,7 +22,18 @@ const Services = () => {
                 </ul>
             </BoxList>
 
-            <MediaVideo content={zeldaVideo} /> 
+            <div className='alinearEnColumna'>
+                <h1>Regional</h1>
+                <div className='enLinea'>
+                    <div className='izquierda'>
+                        <MediaPicture src={regionalImage} alt="placeholder" />
+                    </div>
+
+                </div>
+            </div>
+
+
+            <MediaVideo content={zeldaVideo} />
             <br />
             <MediaVideo content="https://www.youtube.com/watch?v=olKjeHdOW3Y" />
             <br />
