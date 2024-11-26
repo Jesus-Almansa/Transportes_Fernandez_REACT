07ebc7a4f59c75ec80659c1704b1143e03704e18
Jesus-Almansa <jtafernandez@gmail.com>
2024-11-26 11:59:13 +0100

    adaptando MediaPicture a cada situación
    apunto de tocar Box.css para unificar atributos y tener una clase genérica y subclases específicas

--- transportes-fernandez/src/screens/About.jsx
+++ transportes-fernandez/src/screens/About.jsx
@@ -11,9 +11,9 @@ function About() {
   return (
     <div id="About">
 
-      <MediaPicture src={space} alt="placeholder" className='picture-container'/>
+      <MediaPicture src={space} alt="placeholder" className='picture-container' />
 
-      <h1 className='alinearEnColumna'>Cumplimos 70 años</h1>
+      <h1 className='alinearEnColumna'>70 años de historia</h1>
 
       <div className="enLinea">
         <div className="izquierda">
@@ -31,26 +31,10 @@ function About() {
         </div>
       </div>
 
-      <div className="enLinea">
-        <div className="izquierda">
-          <MediaPicture
-            src={laMancha}
-            alt="Descripción de la imagen"
-            className="picture-container picture-empresa-main"
-          />
-
-        </div>
-        <div className='derecha'>
-          <TextBox className='text-box-Empresa'>
-            Transportes Fernández is a family business that has been operating for over 40 years. We specialize in regional transportation, covering the area of La Mancha.
-          </TextBox>
-        </div>
-      </div>
-
 
       <p>Learn more about our company and services. TONTO EL QUE LO LEA</p>
 
-      <h1 className='alinearEnColumna'>Sobre nosotros</h1>
+      <h1 className='alinearEnColumna'>Transportes Fernández a día de hoy</h1>
       <div className="enLinea">
         <div className="izquierda">
           <MediaPicture
@@ -64,6 +48,18 @@ function About() {
           <TextBox className='text-box-Empresa'>
             <p><b>Nacional</b></p>
             Transportes Fernández is a family business that has been operating for over 40 years. We specialize in regional transportation, covering the area of La Mancha.
+          </TextBox>
+        </div>
+      </div>
+      <div id='Resumen' className="alinearEnColumna">
+        <h1>Bienvenido</h1>
+        <div className="enLinea">
+          <TextBox className="text-box-Home-Resumen">
+            Somos una empresa especializada en el transporte de material industrial, con décadas de experiencia ofreciendo soluciones logísticas de alta calidad para grandes empresas del sector. Nos enfocamos exclusivamente en el transporte de maquinaria y materiales industriales, dejando de lado el transporte de paquetes pequeños, lo que nos permite garantizar un servicio altamente especializado, diseñado para cumplir con las necesidades específicas de nuestros clientes.
+
+            Trabajamos para construir una comunidad de personas comprometidas y profesionales, que valoran la excelencia y el crecimiento continuo, tanto a nivel personal como en su trabajo. Aspiramos a superar retos con dedicación, responsabilidad y calidad, siempre garantizando confianza, seguridad y satisfacción en cada servicio que ofrecemos.
+
+            Nuestro objetivo es posicionarnos como un proveedor premium, conocido por nuestra eficiencia, profesionalismo y resultados de calidad. Nos esforzamos por ofrecer un servicio serio, confiable y adaptado a los más altos estándares del sector. Bajo nuestro lema, "Lo primero es el cliente", reafirmamos nuestro compromiso de superar expectativas y construir relaciones sólidas basadas en la confianza y la satisfacción de quienes eligen trabajar con nosotros.
           </TextBox>
         </div>
       </div>
--- transportes-fernandez/src/screens/Home.jsx
+++ transportes-fernandez/src/screens/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 // Componentes
 import Carousel from '../components/features/Carousel/Carousel';
 import BoxList from '../components/layout/Box/BoxList';
-import InfoCard from '../components/layout/Box/InfoCard';
 import EmployeeCard from '../components/features/Employee/EmployeeCard';
 import Background from '../components/features/Background/Background';
 import ContactForm from '../components/features/ContactForm/ContactForm';
@@ -20,7 +19,7 @@ import nacionalImage from '../assets/images/nacional.jpg';
 
 function Home() {
   return (
-    <div>
+    <div id='Home'>
       <Carousel />
 
       <div id='Resumen' className="alinearEnColumna">
@@ -36,18 +35,7 @@ function Home() {
         </div>
       </div>
 
-      <div id='Servicios' className="alinearEnColumna">
-        <h1>Servicios</h1>
-        <div className="enLinea">
-          <TextBox className="text-box-Home-Resumen">
-            Somos una empresa especializada en el transporte de material industrial, con décadas de experiencia ofreciendo soluciones logísticas de alta calidad para grandes empresas del sector. Nos enfocamos exclusivamente en el transporte de maquinaria y materiales industriales, dejando de lado el transporte de paquetes pequeños, lo que nos permite garantizar un servicio altamente especializado, diseñado para cumplir con las necesidades específicas de nuestros clientes.
 
-            Trabajamos para construir una comunidad de personas comprometidas y profesionales, que valoran la excelencia y el crecimiento continuo, tanto a nivel personal como en su trabajo. Aspiramos a superar retos con dedicación, responsabilidad y calidad, siempre garantizando confianza, seguridad y satisfacción en cada servicio que ofrecemos.
-
-            Nuestro objetivo es posicionarnos como un proveedor premium, conocido por nuestra eficiencia, profesionalismo y resultados de calidad. Nos esforzamos por ofrecer un servicio serio, confiable y adaptado a los más altos estándares del sector. Bajo nuestro lema, "Lo primero es el cliente", reafirmamos nuestro compromiso de superar expectativas y construir relaciones sólidas basadas en la confianza y la satisfacción de quienes eligen trabajar con nosotros.
-          </TextBox>
-        </div>
-      </div>
 
 
       <br />
@@ -86,6 +74,19 @@ function Home() {
             position="CEO"
             imageUrl={narutoImage}
           />
+        </div>
+      </div>
+
+      <div id='Servicios' className="alinearEnColumna">
+        <h1>Servicios</h1>
+        <div className="enLinea">
+          <TextBox className="text-box-Home-Resumen">
+            Somos una empresa especializada en el transporte de material industrial, con décadas de experiencia ofreciendo soluciones logísticas de alta calidad para grandes empresas del sector. Nos enfocamos exclusivamente en el transporte de maquinaria y materiales industriales, dejando de lado el transporte de paquetes pequeños, lo que nos permite garantizar un servicio altamente especializado, diseñado para cumplir con las necesidades específicas de nuestros clientes.
+
+            Trabajamos para construir una comunidad de personas comprometidas y profesionales, que valoran la excelencia y el crecimiento continuo, tanto a nivel personal como en su trabajo. Aspiramos a superar retos con dedicación, responsabilidad y calidad, siempre garantizando confianza, seguridad y satisfacción en cada servicio que ofrecemos.
+
+            Nuestro objetivo es posicionarnos como un proveedor premium, conocido por nuestra eficiencia, profesionalismo y resultados de calidad. Nos esforzamos por ofrecer un servicio serio, confiable y adaptado a los más altos estándares del sector. Bajo nuestro lema, "Lo primero es el cliente", reafirmamos nuestro compromiso de superar expectativas y construir relaciones sólidas basadas en la confianza y la satisfacción de quienes eligen trabajar con nosotros.
+          </TextBox>
         </div>
       </div>
 
