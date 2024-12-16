6b8437d1a1bc93508af21303a47ec2e4b78b25d1
Jesús Almansa <jtafernandez@gmail.com>
2024-12-16 14:20:32 +0100

    rutas pa los endpoint

--- transportes-fernandez/src/data/routes.js
+++ transportes-fernandez/src/data/routes.js
@@ -1,7 +1,15 @@
 import Aniversario from '../screens/Aniversario';
+import Empresa from '../screens/Navigation/Empresa';
+import Contacto from '../screens/Navigation/Contacto';
+import Servicios from '../screens/Navigation/Servicios';
+import Equipo from '../screens/Navigation/Equipo';
 
 const routes = [
-  { label: 'Aniversario', path: '/aniversario', component: Aniversario },
+  { label: 'aniversario', path: '/aniversario', component: Aniversario },
+  { label: 'empresa', path: '/empresa', component: Empresa },
+  { label: 'contacto', path: '/contacto', component: Contacto },
+  { label: 'servicios', path: '/servicios', component: Servicios },
+  { label: 'equipo', path: '/equipo', component: Equipo },
 ];
 
 export const findRouteByLabel = (label) =>
