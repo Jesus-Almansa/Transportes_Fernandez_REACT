fa7b4da671e8ee6e7eb2cdc7572f53b521ec8aa1
Jesus-Almansa <jtafernandez@gmail.com>
2024-12-19 18:17:13 +0100

    Al hovear vemos el numero y el correo del empleado

--- transportes-fernandez/src/components/features/Employee/EmployeeCard.jsx
+++ transportes-fernandez/src/components/features/Employee/EmployeeCard.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './EmployeeCard.css';
 
-const EmployeeCard = ({ name, position, imageUrl }) => {
+const EmployeeCard = ({ name, position, imageUrl, number, mail}) => {
     return (
         <div className="employee-card">
             <img src={imageUrl} alt={`${name}`} className="employee-image" />
             <div>
                 <h3 className="employee-name">{name}</h3>
                 <p className="employee-position">{position}</p>
+                <p className="employee-number">{number}</p>
+                <p className="employee-mail">{mail}</p>
             </div>
         </div>
     );
@@ -18,6 +20,8 @@ EmployeeCard.propTypes = {
     name: PropTypes.string.isRequired, // Required name prop
     position: PropTypes.string.isRequired, // Required position prop
     imageUrl: PropTypes.string.isRequired, // Required image URL prop
+    number: PropTypes.string.isRequired,
+    mail: PropTypes.string.isRequired,
 };
 
 export default EmployeeCard;
