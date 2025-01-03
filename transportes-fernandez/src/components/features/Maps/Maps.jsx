7fd71843be26a4340922877570c73c9ccc05e48c
Jesús Almansa <jtafernandez@gmail.com>
2025-01-03 20:03:43 +0100

    mapeado papi

--- transportes-fernandez/src/components/features/Maps/Maps.jsx
+++ transportes-fernandez/src/components/features/Maps/Maps.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
+import './Maps.css'; // Importamos los estilos
 
-function MapLocation() {
+function Maps() {
   return (
-    <div style={{ width: '100%', maxWidth: '1600px', margin: '0 auto' }}>
+    <div className="map-container">
       <iframe
         title="Ubicación - Transportes Fernández"
-        // Armamos la URL manualmente con lat, lng, y un zoom alto (z=17 o 18).
+        // Sustituye este src por el que obtuviste de Google Maps
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1544.2591705616403!2d-2.5895389538417457!3d39.27649388861484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd6887c6bf44c6b1%3A0xbe2e2435eddf6f8c!2sTRANSPORTES%20FERN%C3%81NDEZ!5e0!3m2!1ses!2ses!4v1735929854759!5m2!1ses!2ses"
-        width="100%"
-        height="650"
-        style={{ border: 0 }}
-        allowFullScreen=""
+        className="map-iframe"
+        allowFullScreen
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       />
@@ -18,4 +17,4 @@ function MapLocation() {
   );
 }
 
-export default MapLocation;
+export default Maps;
