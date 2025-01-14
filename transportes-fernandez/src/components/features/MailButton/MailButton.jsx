b8e7a53c6d203cea5e6c61645fb851af7f3c1f15
Jesús Almansa <jtafernandez@gmail.com>
2025-01-14 19:04:24 +0100

    MailButton component, abre una pestaña de gmail al clickarle

---
+++ transportes-fernandez/src/components/features/MailButton/MailButton.jsx
@@ -0,0 +1,41 @@
+// MailButton.jsx
+import React from 'react';
+import './MailButton.css';
+
+// Importa tus dos imágenes
+import gmailIcon from '../../../assets/TransportersBranding/social media icon/1/social media icon-03.png';
+import gmailIconHover from '../../../assets/TransportersBranding/social media icon/3/social media icon-17.png';
+
+const MailButton = ({ email, subject, body, children }) => {
+  const gmailURL = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}${
+    subject ? `&su=${encodeURIComponent(subject)}` : ''
+  }${body ? `&body=${encodeURIComponent(body)}` : ''}`;
+
+  return (
+    <a
+      href={gmailURL}
+      className="mail-button"   // Asigna la clase "mail-button"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {/* Ícono por defecto */}
+      <img
+        src={gmailIcon}
+        alt="Mail Icon Default"
+        className="mail-icon mail-icon-default"
+      />
+
+      {/* Ícono que se mostrará al hacer hover */}
+      <img
+        src={gmailIconHover}
+        alt="Mail Icon Hover"
+        className="mail-icon mail-icon-hover"
+      />
+
+      {/* Texto del botón */}
+      {children}
+    </a>
+  );
+};
+
+export default MailButton;
