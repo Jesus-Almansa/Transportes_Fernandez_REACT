ec5bf9123365df90f2f1542b82131e14f54ec776
Jesús Almansa <jtafernandez@gmail.com>
2025-01-16 18:11:10 +0100

    Botón para volver a todos los servicios desde cualquier endpoint de los servicios

--- transportes-fernandez/src/screens/Endpoints/Industrial.jsx
+++ transportes-fernandez/src/screens/Endpoints/Industrial.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import Button from '../../components/features/Button/Button';
+import MediaPicture from '../../components/features/MediaPicture/MediaPicture';
+import TextBox from '../../components/layout/Box/TextBox';
+
+import { getPathByLabel } from '../../data/routes';
+
 const Industrial = () => {
     return (
         <div>
+
+            {/* <h1>Industrial Endpoint</h1> */}
+
+            <div className='padding-vertical-0'> </div>
+
+            <div className="alinearEnColumna">
+                <Button to={getPathByLabel('servicios')} className={"button button-Servicios-Volver"}>
+                    Volver a servicios
+                </Button>
+            </div>
+
             <h1>Industrial Endpoint</h1>
+
             <p>This is a placeholder for the Industrial screen.</p>
         </div>
     );
