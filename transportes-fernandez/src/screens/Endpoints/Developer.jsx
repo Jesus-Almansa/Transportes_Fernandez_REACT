fb0b902603a9584ed3f5fd5b883be80a62040a98
Jesús Almansa <jtafernandez@gmail.com>
2025-02-05 20:41:36 +0100

    Empezando el desarrollo de /developer, estoy probando alert() messages para felicitar al que lo encuentre

--- transportes-fernandez/src/screens/Endpoints/Developer.jsx
+++ transportes-fernandez/src/screens/Endpoints/Developer.jsx
@@ -1,12 +1,54 @@
-import React from 'react';
+import React from "react";
+import { useEffect, useRef } from "react";
+
+// Components
+import Button from '../../components/features/Button/Button';
+import MediaPicture from '../../components/features/MediaPicture/MediaPicture';
+import TextBox from '../../components/layout/Box/TextBox';
+
+// Imágenes
+import jesus from '../../assets/original_images/dev.jpg';
 
 const Developer = () => {
+
+    const hasAlerted = useRef(false); // Tracks if alert has been shown
+
+    useEffect(() => {
+        if (!hasAlerted.current) {
+            alert("En primer lugar, si has llegado hasta aquí enhorabuena, has encontrado la página secreta de nuestro desarrollador.");
+            hasAlerted.current = true; // Prevents duplicate alerts
+        }
+    }, []);
+
+
     return (
         <div>
-            <h1>Developer Endpoint</h1>
-            <p>This is a placeholder for the Developer screen.</p>
+            <h1 className="large-text-title padding-vertical-3 text-box-Home-IntroTitle">JESÚS TOMÁS ALMANSA FERNÁNDEZ</h1>
+
+            <div className="alinearEnColumna">
+                <div className="enLinea">
+                    <div className="izquierda">
+                        <MediaPicture
+                            src={jesus}
+                            alt="Jose María Fernández"
+                            className="picture-container picture-Jesus"
+                        />
+
+                    </div>
+                    <div className='derecha'>
+                        <TextBox className='text-box text-box-Developer-jesus text-body'>
+
+                            <strong className='text-red'>Jose María </strong> es el socio fundador original de Transportes Fernández. Con más de 40 años  de experiencia en el sector del transporte, ha sido el motor de la empresa desde sus inicios. Su visión y liderazgo han sido fundamentales para el crecimiento y consolidación de la compañía, que ha pasado de ser un pequeño negocio familiar a convertirse en una de las empresas de transporte más reconocidas de la región.
+
+                        </TextBox>
+                    </div>
+                </div>
+            </div>
+
+            <div className='padding-vertical-4'></div>
+
         </div>
     );
-};
+}
 
 export default Developer;
